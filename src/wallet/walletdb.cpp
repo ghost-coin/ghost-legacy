bitcoin/bitcoin

Add permanent option to lockunspent. If true specified outputs are recorded to the wallet database and reloaded at startup. UnlockCoin() will attempt to erase from db if found in set. Including bitcoin PR #13160 - Unlock spent outputs.

src/wallet/walletdb.cpp
@@ -512,6 +512,11 @@ ReadKeyValue(CWallet* pwallet, CDataStream& ssKey, CDataStream& ssValue,
                 strErr = "Error reading wallet database: SetHDChain failed";
                 return false;
             }
+        } else if (strType == "luo")
+        {
+            COutPoint output;
+            ssKey >> output;
+            pwallet->LockCoin(output);
         } else if (strType != "bestblock" && strType != "bestblock_nomerkle"){
             wss.m_unknown_records++;
         }
@@ -919,3 +924,47 @@ bool WalletBatch::WriteVersion(int nVersion)
 {
     return m_batch.WriteVersion(nVersion);
 }
+
+bool WalletBatch::WriteLockedUnspentOutput(const COutPoint &o)
+{
+    bool tmp = true;
+    return WriteIC(std::make_pair(std::string("luo"), o), tmp);
+};
+
+bool WalletBatch::EraseLockedUnspentOutput(const COutPoint &o)
+{
+    return EraseIC(std::make_pair(std::string("luo"), o));
+};
+
+bool WalletBatch::EraseAllByPrefix(std::string sPrefix)
+{
+    // Must be in transaction to call pcursor->del
+    if (!TxnBegin())
+        return error("%s: TxnBegin failed.\n", __func__);
+
+    // Get cursor
+    Dbc *pcursor = nullptr;
+    int ret = m_batch.pdb->cursor(m_batch.activeTxn, &pcursor, 0);
+    if (ret != 0 || !pcursor)
+        return error("%s: GetCursor failed.\n", __func__);
+
+    CDataStream ssKey(SER_DISK, CLIENT_VERSION);
+    CDataStream ssValue(SER_DISK, CLIENT_VERSION);
+    std::string strType;
+    ssKey << sPrefix;
+    while (m_batch.ReadAtCursor(pcursor, ssKey, ssValue, true) == 0)
+    {
+        ssKey >> strType;
+        if (IsKeyType(strType) || strType != sPrefix)
+            break;
+        int rv = pcursor->del(0);
+        if (rv != 0)
+            LogPrintf("%s: Erase failed with error %d\n", __func__, rv);
+        m_database.IncrementUpdateCounter();
+    };
+    pcursor->close();
+
+    TxnCommit();
+
+    return true;
+};

ghost-coin/ghost-legacy

src/wallet/walletdb.cpp
@@ -506,6 +506,11 @@ ReadKeyValue(CWallet* pwallet, CDataStream& ssKey, CDataStream& ssValue,
                 strErr = "Error reading wallet database: SetHDChain failed";
                 return false;
             }
+        } else if (strType == "luo")
+        {
+            COutPoint output;
+            ssKey >> output;
+            pwallet->LockCoin(output);
         }
     } catch (...)
     {
@@ -911,3 +916,47 @@ bool CWalletDB::WriteVersion(int nVersion)
 {
     return batch.WriteVersion(nVersion);
 }
+
+bool CWalletDB::WriteLockedUnspentOutput(const COutPoint &o)
+{
+    bool tmp = true;
+    return WriteIC(std::make_pair(std::string("luo"), o), tmp);
+};
+
+bool CWalletDB::EraseLockedUnspentOutput(const COutPoint &o)
+{
+    return EraseIC(std::make_pair(std::string("luo"), o));
+};
+
+bool CWalletDB::EraseAllByPrefix(std::string sPrefix)
+{
+    // Must be in transaction to call pcursor->del
+    if (!TxnBegin())
+        return error("%s: TxnBegin failed.\n", __func__);
+
+    // Get cursor
+    Dbc *pcursor = nullptr;
+    int ret = batch.pdb->cursor(batch.activeTxn, &pcursor, 0);
+    if (ret != 0 || !pcursor)
+        return error("%s: GetCursor failed.\n", __func__);
+
+    CDataStream ssKey(SER_DISK, CLIENT_VERSION);
+    CDataStream ssValue(SER_DISK, CLIENT_VERSION);
+    std::string strType;
+    ssKey << sPrefix;
+    while (batch.ReadAtCursor(pcursor, ssKey, ssValue, true) == 0)
+    {
+        ssKey >> strType;
+        if (IsKeyType(strType) || strType != sPrefix)
+            break;
+        int rv = pcursor->del(0);
+        if (rv != 0)
+            LogPrintf("%s: Erase failed with error %d\n", __func__, rv);
+        m_dbw.IncrementUpdateCounter();
+    };
+    pcursor->close();
+
+    TxnCommit();
+
+    return true;
+};