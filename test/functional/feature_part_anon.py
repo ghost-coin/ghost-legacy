0ee440c0e4f9f12b0f7650caff50986d167a026f
tecnovert <tecnovert@particl.io>
2019-08-02 13:28:44 +0200

    tests: Add test for recover from mnemonic.

--- test/functional/feature_part_anon.py
+++ test/functional/feature_part_anon.py
@@ -38,24 +38,12 @@ class AnonTest(ParticlTestFramework):
 
         sxAddrTo0_1 = nodes[0].getnewstealthaddress('lblsx01')
 
-
-        txnHash = nodes[0].sendparttoanon(sxAddrTo1_1, 1, '', '', False, 'node0 -> node1 p->a')
-        txnHashes.append(txnHash)
-
-        txnHash = nodes[0].sendparttoblind(sxAddrTo0_1, 1000, '', '', False, 'node0 -> node0 p->b')
-        txnHashes.append(txnHash)
-
-        txnHash = nodes[0].sendblindtoanon(sxAddrTo1_1, 100, '', '', False, 'node0 -> node1 b->a 1')
-        txnHashes.append(txnHash)
-
-        txnHash = nodes[0].sendblindtoanon(sxAddrTo1_1, 100, '', '', False, 'node0 -> node1 b->a 2')
-        txnHashes.append(txnHash)
-
-        txnHash = nodes[0].sendblindtoanon(sxAddrTo1_1, 100, '', '', False, 'node0 -> node1 b->a 3')
-        txnHashes.append(txnHash)
-
-        txnHash = nodes[0].sendblindtoanon(sxAddrTo1_1, 10, '', '', False, 'node0 -> node1 b->a 4')
-        txnHashes.append(txnHash)
+        txnHashes.append(nodes[0].sendparttoanon(sxAddrTo1_1, 1, '', '', False, 'node0 -> node1 p->a'))
+        txnHashes.append(nodes[0].sendparttoblind(sxAddrTo0_1, 1000, '', '', False, 'node0 -> node0 p->b'))
+        txnHashes.append(nodes[0].sendblindtoanon(sxAddrTo1_1, 100, '', '', False, 'node0 -> node1 b->a 1'))
+        txnHashes.append(nodes[0].sendblindtoanon(sxAddrTo1_1, 100, '', '', False, 'node0 -> node1 b->a 2'))
+        txnHashes.append(nodes[0].sendblindtoanon(sxAddrTo1_1, 100, '', '', False, 'node0 -> node1 b->a 3'))
+        txnHashes.append(nodes[0].sendblindtoanon(sxAddrTo1_1, 10, '', '', False, 'node0 -> node1 b->a 4'))
 
         for k in range(4):
             txnHash = nodes[0].sendparttoanon(sxAddrTo1_1, 10, '', '', False, 'node0 -> node1 p->a')
@@ -78,15 +66,10 @@ class AnonTest(ParticlTestFramework):
         for txnHash in txnHashes:
             assert(txnHash in ro['tx'])
 
-
         txnHash = nodes[1].sendanontoanon(sxAddrTo0_1, 1, '', '', False, 'node1 -> node0 a->a')
         txnHashes = [txnHash,]
 
         assert(self.wait_for_mempool(nodes[0], txnHash))
-
-        ro = nodes[0].listtransactions()
-        #print("0 listtransactions ", json.dumps(ro, indent=4, default=self.jsonDecimal))
-
         self.stakeBlocks(1)
 
         block1_hash = nodes[1].getblockhash(2)
@@ -94,29 +77,22 @@ class AnonTest(ParticlTestFramework):
         for txnHash in txnHashes:
             assert(txnHash in ro['tx'])
 
-        ro = nodes[1].anonoutput()
-        assert(ro['lastindex'] == 26)
+        assert(nodes[1].anonoutput()['lastindex'] == 26)
 
-        txnHash = nodes[1].sendanontoanon(sxAddrTo0_1, 101, '', '', False, 'node1 -> node0 a->a', 5, 1)
-        txnHashes = [txnHash,]
+        txnHashes.clear()
+        txnHashes.append(nodes[1].sendanontoanon(sxAddrTo0_1, 101, '', '', False, 'node1 -> node0 a->a', 5, 1))
+        txnHashes.append(nodes[1].sendanontoanon(sxAddrTo0_1, 0.1, '', '', False, '', 5, 2))
 
-        assert(self.wait_for_mempool(nodes[0], txnHash))
-
-        txnHash = nodes[1].sendanontoanon(sxAddrTo0_1, 0.1, '', '', False, '', 5, 2)
-        txnHashes = [txnHash,]
-
-        assert(self.wait_for_mempool(nodes[0], txnHash))
-
-
-        ro = nodes[1].getwalletinfo()
-        assert(ro['anon_balance'] > 10)
+        assert(nodes[1].getwalletinfo()['anon_balance'] > 10)
 
         outputs = [{'address':sxAddrTo0_1, 'amount':10, 'subfee':True},]
         ro = nodes[1].sendtypeto('anon', 'part', outputs, 'comment_to', 'comment_from', 4, 32, True)
         assert(ro['bytes'] > 0)
 
-        txnHash = nodes[1].sendtypeto('anon', 'part', outputs)
-        txnHashes = [txnHash,]
+        txnHashes.append(nodes[1].sendtypeto('anon', 'part', outputs))
+
+        for txhash in txnHashes:
+            assert(self.wait_for_mempool(nodes[0], txhash))
 
         self.log.info('Test filtertransactions with type filter')
         ro = nodes[1].filtertransactions({ 'type': 'anon', 'count': 20 })
@@ -141,6 +117,7 @@ class AnonTest(ParticlTestFramework):
         # Restart node
         self.stop_node(1)
         self.start_node(1, self.extra_args[1])
+        connect_nodes_bi(self.nodes, 0, 1)
         assert(len(nodes[1].listlockunspent()) == 2)
         assert(len(nodes[1].listunspentanon()) < len(unspent))
         assert(nodes[1].lockunspent(True, [unspent[0]]) == True)
@@ -152,6 +129,39 @@ class AnonTest(ParticlTestFramework):
 
         ro = nodes[2].getblockstats(nodes[2].getblockchaininfo()['blocks'])
         assert(ro['height'] == 2)
+
+        self.log.info('Test recover from mnemonic')
+        # Txns currently in the mempool will be reprocessed in thenext block
+        self.sync_all()
+        self.stakeBlocks(1)
+        wi_1 = nodes[1].getwalletinfo()
+
+        nodes[1].createwallet('test_import')
+        w1_2 = nodes[1].get_wallet_rpc('test_import')
+        w1_2.extkeyimportmaster('drip fog service village program equip minute dentist series hawk crop sphere olympic lazy garbage segment fox library good alley steak jazz force inmate')
+        w1_2.getnewstealthaddress('lblsx11')
+        w1_2.rescanblockchain(0)
+        wi_1_2 = w1_2.getwalletinfo()
+        assert(wi_1_2['anon_balance'] == wi_1['anon_balance'])
+
+        nodes[1].createwallet('test_import_locked')
+        w1_3 = nodes[1].get_wallet_rpc('test_import_locked')
+        w1_3.encryptwallet('test')
+        w1_3.walletpassphrase('test', 30)
+
+        # Skip initial rescan by passing -1 as scan_chain_from
+        w1_3.extkeyimportmaster('drip fog service village program equip minute dentist series hawk crop sphere olympic lazy garbage segment fox library good alley steak jazz force inmate',
+            '', False, 'imported key', 'imported acc', -1)
+        w1_3.getnewstealthaddress('lblsx11')
+        w1_3.walletsettings('other', {'onlyinstance': False})
+        w1_3.walletlock()
+        assert(w1_3.getwalletinfo()['encryptionstatus'] == 'Locked')
+        w1_3.rescanblockchain(0)
+
+        w1_3.walletpassphrase('test', 30)
+
+        wi_1_3 = w1_3.getwalletinfo()
+        assert(wi_1_3['anon_balance'] == wi_1['anon_balance'])
 
 
 if __name__ == '__main__':
