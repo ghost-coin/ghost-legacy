41c7840bc95d3fae0d064292f28f2532f0a81846
tecnovert <tecnovert@particl.io>
2019-08-28 13:44:42 +0200

    test: Remove unnecessary code. g_connman is initialised in TestingSetup.

--- src/wallet/test/stake_tests.cpp
+++ src/wallet/test/stake_tests.cpp
@@ -253,7 +253,6 @@ BOOST_AUTO_TEST_CASE(stake_test)
         BOOST_CHECK(pwallet->CreateTransaction(*locked_chain, vecSend, tx_new, reservekey, nFeeRequired, nChangePosRet, strError, coinControl));
     }
     {
-        g_connman = std::unique_ptr<CConnman>(new CConnman(GetRand(std::numeric_limits<uint64_t>::max()), GetRand(std::numeric_limits<uint64_t>::max())));
         CValidationState state;
         pwallet->SetBroadcastTransactions(true);
         mapValue_t mapValue;
