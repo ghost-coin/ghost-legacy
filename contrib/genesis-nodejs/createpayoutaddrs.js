0798ba44ad28d95815103370b3ee0bc488a503b9
Reborn1002 <rebornonezerozero@gmail.com>
2020-05-28 21:56:29 -0700

    centralize config

--- contrib/genesis-nodejs/createpayoutaddrs.js
+++ contrib/genesis-nodejs/createpayoutaddrs.js
@@ -9,36 +9,26 @@ commander
   .option('-n, --network <name>', 'Network to generate for', 'mainnet')
   .parse(process.argv);
 
-function getRPCPortforNet(net){
-    switch (net){
-        case 'mainnet':
-            return 51725;
-        case 'testnet':
-            return 51925;
-        case 'regtest':
-            return 51926;
-    }
-}
+const network = commander.network;
+
+/*
+Config consts and coin related consts
+*/
+const configNetwork = require('./config.json').networks[network];
 
 /*
 Main func
 */
 var GetNewAddresses = function() {
+    
     var RpcClient = require('bitcoind-rpc');
     var addrs = [];
     const addrsneeded = 20;
-    var config = {
-      protocol: 'http',
-      user: 'user',
-      pass: 'pass',
-      host: '127.0.0.1',
-      port: getRPCPortforNet(commander.network),
-    };
-  
+    
     // config can also be an url, e.g.:
     // var config = 'http://user:pass@127.0.0.1:18332';
-  
-    var rpc = new RpcClient(config);  
+    
+    var rpc = new RpcClient(configNetwork.config);  
     function getPaymentaddresses() {
         for(var i=0;i<addrsneeded;i++){
             rpc.getNewAddress(function (err, ret) {
