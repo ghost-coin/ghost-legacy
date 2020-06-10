fd5a04022e91508824c21838ea1e040e74dd1bdf
AkshayCM <akshaycm@hotmail.com>
2020-06-10 21:21:35 +0300

    Update Particl core copyright year

--- src/script/ismine.h
+++ src/script/ismine.h
@@ -1,6 +1,6 @@
 // Copyright (c) 2009-2010 Satoshi Nakamoto
 // Copyright (c) 2009-2019 The Bitcoin Core developers
-// Copyright (c) 2017-2019 The Particl Core developers
+// Copyright (c) 2017-2020 The Particl Core developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
