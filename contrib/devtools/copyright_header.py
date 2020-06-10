f9112f8a6590fd47368a72f8f659f8317d60263b
AkshayCM <akshaycm@hotmail.com>
2020-06-10 21:17:39 +0300

    Copyright year update in copyright of Bitcoin Core

--- contrib/devtools/copyright_header.py
+++ contrib/devtools/copyright_header.py
@@ -108,6 +108,7 @@ EXPECTED_HOLDER_NAMES = [
     r"The BlackCoin developers\n",
     r"The ShadowCoin developers\n",
     r"The Particl Core developers\n",
+    r"The Ghost Core developers\n",
 ]
 
 DOMINANT_STYLE_COMPILED = {}
