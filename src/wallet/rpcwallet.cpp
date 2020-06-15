57e7782a4609caf138d1188f460886bfa10401d9
AkshayCM <akshaycm@hotmail.com>
2020-06-15 03:24:04 +0300

    Fix sending via sendtoaddress RPC

--- src/wallet/rpcwallet.cpp
+++ src/wallet/rpcwallet.cpp
@@ -576,8 +576,8 @@ static UniValue sendtoaddress(const JSONRPCRequest& request)
         newRequest.fSkipBlock = true; // already blocked in this function
         newRequest.URI = request.URI;
         UniValue params(UniValue::VARR);
-        params.push_back("part");
-        params.push_back("part");
+        params.push_back("ghost");
+        params.push_back("ghost");
         UniValue arr(UniValue::VARR);
         UniValue out(UniValue::VOBJ);
 
@@ -1110,8 +1110,8 @@ static UniValue sendmany(const JSONRPCRequest& request)
         newRequest.fSkipBlock = true; // already blocked in this function
         newRequest.URI = request.URI;
         UniValue params(UniValue::VARR);
-        params.push_back("part");
-        params.push_back("part");
+        params.push_back("ghost");
+        params.push_back("ghost");
         UniValue arr(UniValue::VARR);
 
         std::vector<std::string> keys = sendTo.getKeys();
