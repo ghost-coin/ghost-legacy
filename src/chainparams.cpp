0af04ab939bda18d1dbb242a08b3e023be140218
AkshayCM <akshaycm@hotmail.com>
2020-06-18 01:39:29 +0300

    Change pchmessage to not conflict with Particl

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -399,9 +399,9 @@ public:
          * The characters are rarely used upper ASCII, not valid as UTF-8, and produce
          * a large 32-bit integer with any alignment.
          */
-        pchMessageStart[0] = 0xfb;
-        pchMessageStart[1] = 0xf2;
-        pchMessageStart[2] = 0xef;
+        pchMessageStart[0] = 0xf2;
+        pchMessageStart[1] = 0xf3;
+        pchMessageStart[2] = 0xe1;
         pchMessageStart[3] = 0xb4;
         nDefaultPort = 51728;
         nBIP44ID = 0x8000002C;
