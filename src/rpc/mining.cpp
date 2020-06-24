ac872ca15a694957e141e61a4205e690a332aa16
akshaynexus <akshaycm@hotmail.com>
2020-06-24 19:48:43 +0000

    Cleanup of particl mentions

--- src/rpc/mining.cpp
+++ src/rpc/mining.cpp
@@ -146,7 +146,7 @@ static UniValue generatetoaddress(const JSONRPCRequest& request)
                 "\nMine blocks immediately to a specified address (before the RPC call returns)\n",
                 {
                     {"nblocks", RPCArg::Type::NUM, RPCArg::Optional::NO, "How many blocks are generated immediately."},
-                    {"address", RPCArg::Type::STR, RPCArg::Optional::NO, "The address to send the newly generated particl to."},
+                    {"address", RPCArg::Type::STR, RPCArg::Optional::NO, "The address to send the newly generated ghost to."},
                     {"maxtries", RPCArg::Type::NUM, /* default */ "1000000", "How many iterations to try."},
                 },
                 RPCResult{
