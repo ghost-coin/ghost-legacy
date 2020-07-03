df69d0ba3506293b7709139fd282f779fa97f8e6
akshaynexus <akshaycm@hotmail.com>
2020-07-03 22:05:39 +0000

    Gitian changes

--- contrib/gitian-scripts/compileversion.sh
+++ contrib/gitian-scripts/compileversion.sh
@@ -1,4 +1,4 @@
-export VERSION=0.19.1.2
-export SIGNER='Ghost'
+export VERSION=0.19.1.6
+export SIGNER='akshaynexus'
 export LXC_BRIDGE=lxcbr0
-./gitian-build.py --build --no-commit -j 32 -m 40000 $SIGNER $VERSION
+./gitian-build.py --build --no-commit -j 32 -m 64000 $SIGNER $VERSION
