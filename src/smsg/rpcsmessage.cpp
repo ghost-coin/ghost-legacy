e60135ea84530b7e389bd929d0e0d80c396bbe63
tecnovert <tecnovert@tecnovert.net>
2021-01-11 02:06:14 +0200

    Fix smsginbox hex encoding bug.

--- src/smsg/rpcsmessage.cpp
+++ src/smsg/rpcsmessage.cpp
@@ -1139,7 +1139,7 @@ static UniValue smsginbox(const JSONRPCRequest &request)
                         objM.pushKV("text", sText);
                     } else
                     if (sEnc == "hex") {
-                        objM.pushKV("hex", HexStr(sText));
+                        objM.pushKV("hex", HexStr(msg.vchMessage));
                     } else {
                         objM.pushKV("unknown_encoding", sEnc);
                     }
@@ -1301,7 +1301,7 @@ static UniValue smsgoutbox(const JSONRPCRequest &request)
                         objM.pushKV("text", sText);
                     } else
                     if (sEnc == "hex") {
-                        objM.pushKV("hex", HexStr(sText));
+                        objM.pushKV("hex", HexStr(msg.vchMessage));
                     } else {
                         objM.pushKV("unknown_encoding", sEnc);
                     }
