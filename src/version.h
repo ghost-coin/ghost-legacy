b749819dec87ffd9f20fd8c428c83206f0a61cbc
tecnovert <tecnovert@tecnovert.net>
2021-06-22 00:16:18 +0200

    net: Raise Protocol version and minimum protocol version.

--- src/version.h
+++ src/version.h
@@ -9,13 +9,13 @@
  * network protocol versioning
  */
 
-static const int PROTOCOL_VERSION = 90012;
+static const int PROTOCOL_VERSION = 90013;
 
 //! initial proto version, to be increased after version/verack negotiation
 static const int INIT_PROTO_VERSION = 209;
 
 //! disconnect from peers older than this proto version
-static const int MIN_PEER_PROTO_VERSION = 90009;
+static const int MIN_PEER_PROTO_VERSION = 90012;
 
 //! nTime field added to CAddress, starting with this version;
 //! if possible, avoid requesting addresses nodes older than this
